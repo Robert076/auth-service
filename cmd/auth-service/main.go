344dfd5f11e8fe89b09114c95e75ddecb4b90ffb
Robert076 <robertberes06@gmail.com>
2025-06-24 08:43:12 +0300

    linter fix

--- cmd/auth-service/main.go
+++ cmd/auth-service/main.go
@@ -14,6 +14,7 @@ import (
 )
 
 func main() {
+	// hello
 	if os.Getenv("ENVIRONMENT") != "PRODUCTION" {
 		if err := godotenv.Load(); err != nil {
 			log.Fatalf("%s: Failed loading env file", constants.ServiceName)
