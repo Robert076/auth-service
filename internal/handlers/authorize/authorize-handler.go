f36239fd97c2d8225c731c78fd68bb9307203729
Robert076 <robertberes06@gmail.com>
2025-07-03 09:54:32 +0300

    error validation for http request type

--- internal/handlers/authorize/authorize-handler.go
+++ internal/handlers/authorize/authorize-handler.go
@@ -1,13 +1,20 @@
 package authorize_handler
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/Robert076/auth-service/internal/db/repository"
+	validation_service "github.com/Robert076/auth-service/internal/service/validation-service"
 )
 
 func AuthorizeHandler(repo repository.IRepository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if err := validation_service.IsValidHttpRequest(r, http.MethodPost); err != nil {
+			http.Error(w, "This endpoint only accepts POST requests", http.StatusBadRequest)
+			log.Printf("This endpoint only accepts POST requests: %v", err)
+			return
+		}
 
 	}
 }
