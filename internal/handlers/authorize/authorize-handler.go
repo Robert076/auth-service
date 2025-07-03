7bd34b66a22a976896683c07f694d25be8bce937
Robert076 <robertberes06@gmail.com>
2025-07-03 10:05:01 +0300

    more validation, need to add method to repo interface

--- internal/handlers/authorize/authorize-handler.go
+++ internal/handlers/authorize/authorize-handler.go
@@ -1,11 +1,13 @@
 package authorize_handler
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/Robert076/auth-service/internal/db/repository"
 	validation_service "github.com/Robert076/auth-service/internal/service/validation-service"
+	"github.com/Robert076/auth-service/internal/user"
 )
 
 func AuthorizeHandler(repo repository.IRepository) http.HandlerFunc {
@@ -15,6 +17,18 @@ func AuthorizeHandler(repo repository.IRepository) http.HandlerFunc {
 			log.Printf("This endpoint only accepts POST requests: %v", err)
 			return
 		}
+		var u user.AuthorizeUserDTO
 
+		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+			http.Error(w, "Error decoding body", http.StatusBadRequest)
+			log.Printf("Error decoding JSON body for authorization: %v", err)
+			return
+		}
+
+		if err := repo.GetUserByEmail(u.Email); err != nil {
+			http.Error(w, "Error retrieving user from db", http.StatusBadRequest)
+			log.Printf("Error retrieving user from db: %v", err)
+			return
+		}
 	}
 }
